cba8507805a153035ca33826d955385ee9163e77
Shweta Sharma <shwetash1704@gmail.com>
2022-10-14 15:53:01 +0530

    lightDark

--- home.js
+++ home.js
@@ -7,3 +7,8 @@ function closeCoupon(){
     document.getElementById('coupon').style.visibility = 'hidden';
     document.getElementById('myCarousel').style.opacity='1'
 }
+
+function changeMode(){
+    let mybody = document.body;
+    mybody.classList.toggle('mydark')
+}
